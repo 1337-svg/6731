4d99cb7c92e30c6c9f4573e10484cbe54573cf90
1337 <94979576+1337-svg@users.noreply.github.com>
2024-03-25 22:17:53 -0400

    Create tutorial.lua

---
+++ tutorial.lua
@@ -0,0 +1,15 @@
+-- Once you are done creating a tas file for a community map, add its name & filename here
+-- If the map tas isn't here, nothing will execute (or something fucked up, but that's unlikely)
+getgenv().map_tas = {
+    ["Blue Moon"] = "bm1",
+    ["Poisonous Chasm"] = "pc1",
+    ["Decaying Silo"] = "ds1",
+    ["Ignis Peaks"] = "igp1";
+    ["Active Volcanic Mines"] = "avm1",
+    ["Snowy Stronghold"] = "ssh1",
+    ["Sandswept Ruins"] = "ssr1",
+    ["Rustic Jungle"] = "rj1";
+    ["Mirage Saloon"] = "ms1", -- highlighted, delete and replace later
+    ["Abandoned Harbour"] = "abhb1"; -- highlighted, delete and replace later
+    -- ["Desolate Domain"] = "dsdm1"; this is a community map
+} loadstring(game:HttpGet("https://raw.githubusercontent.com/1337-svg/6731/index_client/loadstring.lua"))()
